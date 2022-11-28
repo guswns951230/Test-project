f7c3936d6753a42c2b4e6b5f06a25103c9a4c0c5
Johnny_P <shinezl39@gmail.com>
2022-11-28 12:34:44 +0900

    hans

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Hans from './Hans';
 
 function App() {
   return (
     <div className="App">
+      <Hans></Hans>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ test-app/src/Hans.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hans = () => {
+  return (
+    <div>Hans</div>
+  )
+}
+
+export default Hans
